0cae6c8d31f7e1499d1b4f007940f3820f801f1c
careyarmst <careyarmst@earthlink.net>
2024-11-21 20:58:41 -0500

    Commit VueSo Far

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
   <div id="app">
-    
+   <messagesMword/> 
   </div>
 </template>
 
 <script>
-import Messages from './components/Messages.vue'
+import MessagesMword from './components/messagesMword.vue'
+
 
 export default {
   name: 'App',
   components: {
-    Messages
+    MessagesMword
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/messagesMword.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+<h4>Messages</h4>
+<ul v-for="(message,index) in messages" v-bind:key="index">
+    <li>{{message}}</li>
+    </ul>
+</div> 
+</template>
+<script>
+export default {
+    data() {
+      return {
+        messages: ["hello", "hi", "its working"]
+      }
+}
+}
+</script>
+
